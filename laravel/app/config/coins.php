c11325b1d318c9e169d244f9d9acd332cf68669e
Matt Horning <matt.horning@gmail.com>
2014-09-28 10:35:45 -0500

    added in new coins.config

---
+++ laravel/app/config/coins.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'DOGE' => array(
+		'IP'   => '192.168.1.5',
+		'PORT' => '44555',
+		'USER' => 'snollygolly',
+		'PASS' => 'COIND PASSWORD GOES HERE',
+		'CURRENCY' => 'DOGE',
+		'CURRENCY_FULL' => 'Dogecoin',
+		'CURRENCY_IMG' => '/img/dogecoin-d-16.png',
+		'MIN_CONF' => 3,
+		'TX_FEE' => 1
+	)
+);
